a249c2c99b31b29fa0fd6d7887430cd549da01e1
lucyygao <42822456+lucyygao@users.noreply.github.com>
2021-05-31 17:20:32 -0700

    buying powerups in shop

--- include/shop.h
+++ include/shop.h
@@ -25,3 +25,15 @@
 #include "preferences.h"
 
 scene_t *make_shop_scene ();
+
+void *shop_mouse_click();
+
+void buy_immunity();
+
+void buy_magnet();
+
+void buy_boost();
+
+int get_star_count();
+void change_star_count(int new_count);
+void write_powerup(char *powerup);
