2041c33b720579048093d6d57c1a3efa9fc01d4e
emleee <emlee2002@gmail.com>
2021-06-01 19:29:06 -0700

    more sliding platforms and deleting includes

--- demo/doodlejump.c
+++ demo/doodlejump.c
@@ -1,28 +1,7 @@
-#include <stddef.h>
-#include <math.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <string.h>
-#include "polygon.h"
-#include "color.h"
-#include "list.h"
-#include <time.h>
-#include <stdio.h>
-#include "forces.h"
-#include "collision.h"
-#include "platforms.h"
-#include "sdl_wrapper.h"
-#include "game_sprites.h"
-#include "SDL2/SDL_mixer.h"
-#include "text.h"
-#include "test_util.h"
-#include "powerups.h"
-#include "star.h"
 // Different Screens
 #include "game.h"
 #include "restart.h"
 #include "start.h"
-#include "preferences.h"
 #include "shop.h"
 
 const double WIDTH2 = 720.0;
