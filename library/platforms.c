b7fb0e90b07f4eb32418644a796f0c674ab88cc6
tzhang3 <tzhang3@caltech.edu>
2021-06-07 16:10:37 -0400

    clean platforms

--- library/platforms.c
+++ library/platforms.c
@@ -1,18 +1,10 @@
 #include "polygon.h"
 #include "sdl_wrapper.h"
 #include "platforms.h"
+#include "constants.h"
 
-const double WINDOW_LENGTH = 720.0;
-
-// const double PLATFORM_WIDTH = 146;
-// const double PLATFORM_HEIGHT = 35;
 const double PLATFORM_MASS = INFINITY;
 const double TRICK_MASS = 0;
-
-const rgb_color_t NORMAL_COLOR = {.r = 0.0/255.0, .g = 204.0/255.0, .b = 0.0/255.0};
-const rgb_color_t SLIDING_COLOR = {.r = 77.0/255.0, .g = 166.0/255.0, .b = 255.0/255.0};
-const rgb_color_t DISAPPEARING_COLOR = {.r = 255.0/255.0, .g = 204.0/255.0, .b = 255.0/255.0};
-const rgb_color_t TRICK_COLOR = {.r = 102.0/255.0, .g = 51.0/255.0, .b = 0.0/255.0};
 
 const vector_t NORMAL_DIMENSIONS = {.x = 146, .y = 35};
 const vector_t SLIDING_DIMENSIONS = {.x = 146, .y = 35};
@@ -23,21 +15,20 @@ const vector_t PLATFORM_VELOCITY = {.x = 200, .y = 0};
 
 body_t *make_platform(vector_t center, rgb_color_t color, char *info, int width, int height, sprite_t *sprite) {
     list_t *shape = make_rectangle(VEC_ZERO, width, height);
-    body_t *platform = body_init_with_info(shape, PLATFORM_MASS, color, info, free);
-    body_set_sprite(platform, sprite);
+    body_t *platform = body_init_with_sprite(shape, PLATFORM_MASS, color, info, free, sprite);
     body_set_centroid(platform, center);
     return platform;
 }
 
 body_t *normal_platform(vector_t center, char *info) {
     sprite_t *sprite = create_sprite("PNGs/Grass_Platform.png", NORMAL_DIMENSIONS.x, NORMAL_DIMENSIONS.y);
-    body_t *platform = make_platform(center, NORMAL_COLOR, info, NORMAL_DIMENSIONS.x, NORMAL_DIMENSIONS.y, sprite);
+    body_t *platform = make_platform(center, SPRITE_COLOR, info, NORMAL_DIMENSIONS.x, NORMAL_DIMENSIONS.y, sprite);
     return platform;
 }
 
 body_t *sliding_platform(vector_t center, char *info) {
     sprite_t *sprite = create_sprite("PNGs/Flower_Platform.png", SLIDING_DIMENSIONS.x, SLIDING_DIMENSIONS.y);
-    body_t *sliding_platform = make_platform(center, SLIDING_COLOR, info, SLIDING_DIMENSIONS.x, SLIDING_DIMENSIONS.y, sprite);
+    body_t *sliding_platform = make_platform(center, SPRITE_COLOR, info, SLIDING_DIMENSIONS.x, SLIDING_DIMENSIONS.y, sprite);
     body_set_velocity(sliding_platform, PLATFORM_VELOCITY);
     return sliding_platform;
 }
@@ -46,8 +37,8 @@ void sliding_bounce(body_t *sliding_platform) {
     vector_t adjust = VEC_ZERO;
     list_t *shape = body_get_shape(sliding_platform);
     vector_t velocity = body_get_velocity(sliding_platform);
+    
     vector_t *worst_pt = list_get(shape, 0);
-
     for (size_t i = 1; i < list_size(shape); i++) {
         vector_t *pt = list_get(shape, i);
         if ((velocity.x < 0 && pt->x < worst_pt->x) || (velocity.x > 0 && pt->x > worst_pt->x))
@@ -61,8 +52,8 @@ void sliding_bounce(body_t *sliding_platform) {
         adjust.x = -worst_pt->x;
         velocity.x *= -1;
     }
-    else if (worst_pt->x > WINDOW_LENGTH) {
-        adjust.x = WINDOW_LENGTH - worst_pt->x;
+    else if (worst_pt->x > SCREEN_DIMENSIONS.x) {
+        adjust.x = SCREEN_DIMENSIONS.x - worst_pt->x;
         velocity.x *= -1;
     }
 
@@ -73,13 +64,13 @@ void sliding_bounce(body_t *sliding_platform) {
 
 body_t *disappearing_platform(vector_t center, char *info) {
     sprite_t *sprite = create_sprite("PNGs/Cloud_Platform.png", DISAPPEARING_DIMENSIONS.x, DISAPPEARING_DIMENSIONS.y);
-    body_t *disappearing_platform = make_platform(center, DISAPPEARING_COLOR, info, DISAPPEARING_DIMENSIONS.x, DISAPPEARING_DIMENSIONS.y, sprite);
+    body_t *disappearing_platform = make_platform(center, SPRITE_COLOR, info, DISAPPEARING_DIMENSIONS.x, DISAPPEARING_DIMENSIONS.y, sprite);
     return disappearing_platform;
 }
 
 body_t *trick_platform(vector_t center, char *info) {
     sprite_t *sprite = create_sprite("PNGs/Branch_Platform.png", TRICK_DIMENSIONS.x, TRICK_DIMENSIONS.y);
-    body_t *trick_platform = make_platform(center, DISAPPEARING_COLOR, info, TRICK_DIMENSIONS.x, TRICK_DIMENSIONS.y, sprite);
+    body_t *trick_platform = make_platform(center, SPRITE_COLOR, info, TRICK_DIMENSIONS.x, TRICK_DIMENSIONS.y, sprite);
     body_set_mass(trick_platform, TRICK_MASS);
     return trick_platform;
 }
